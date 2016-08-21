ff6b884457912c93789ae1c786be1274b12a0e1e
Darryl Blake <darryl@darrylblake.com>
2016-08-21 22:33:10 +1200

    Messing around with Enzyme/tests.

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Tour from './Tour'
 
 export default class Main extends Component {
   render() {
     return (
       <div id="container">
-        <h1>Interactive Labs</h1>
+        <h1>Heading here...</h1>
+        <h2>Heading here...</h2>
+        <Tour />
         {this.props.children}
       </div>
     );
--- test/Main-test.js
+++ test/Main-test.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import { expect } from 'chai';
+import { describe, it } from 'mocha';
 import { shallow, mount, render } from 'enzyme';
 import Main from '../src/containers/Main';
+import Tour from '../src/containers/Tour';
 
-describe("<Main />", function() {
-  it("Should have a div container with id: 'container'", function() {
-    expect(shallow(<Main />).find('div#container').length).to.equal(1);
+describe('<Main /> container', function() {
+  const wrapper = mount(<Main />);
+
+  it("Should have a div container with id: 'container'", () => {
+    expect(wrapper.find('div#container').length).to.equal(1);
   });
 
-  it("Should have an H1 title", function() {
-    expect(shallow(<Main />).find('h1').text()).to.equal('Interactive Labs');
+  describe('<h1> should be', () => {
+    it('present', () => {
+      expect(wrapper.containsMatchingElement(<h1>Heading here...</h1>)) .to.equal(true);
+      // expect(wrapper.find('h1').length) .to.equal(1);
+    });
+    it('should not be empty', () => {
+      expect(wrapper.find('h1').text().length).to.above(0);
+    });
+  });
+
+  it('Should contain a <Tour /> node', () => {
+    expect(wrapper.contains(<Tour />)).to.equal(true);
   });
 });
